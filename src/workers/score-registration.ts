d04d014290292e3f52bc4d46cd3029bb0a23b8f4
tekihei2317 <tekihei2317@gmail.com>
2025-06-24 18:58:00 +0900

    スコアが存在するかどうか判定する関数を、データ取得処理を抽象化して書いた

---
+++ src/workers/score-registration.ts
@@ -0,0 +1,82 @@
+export type RankingEntry = { userName: string; score: number };
+
+export type FetchRankingByPage = (page: number) => Promise<RankingEntry[]>;
+
+type HasEtypingScoreInput = {
+  userData: { userName: string; score: number };
+  fetchRankingByPage: FetchRankingByPage;
+  pageNum: number;
+};
+
+/**
+ * そのスコアの記録が存在する可能性がある、最初のページを取得する
+ */
+async function findPageByScore({
+  score,
+  fetchRankingByPage,
+  pageNum,
+}: {
+  score: number;
+  fetchRankingByPage: FetchRankingByPage;
+  pageNum: number;
+}): Promise<number> {
+  // min(pageのスコア) <= scoreとなる最小のpageを求める
+  // 1-indexed
+  let ok = pageNum + 1;
+  let ng = 0;
+  while (ok - ng > 1) {
+    const mid = Math.floor((ng + ok) / 2);
+
+    const entries = await fetchRankingByPage(mid);
+    const minScore = Math.min(...entries.map((entry) => entry.score));
+
+    if (minScore <= score) ok = mid;
+    else ng = mid;
+  }
+  return ok;
+}
+
+/**
+ * ランキングの配列に、定のユーザーのデータが存在するか判定する
+ */
+function userDataExists(
+  userData: { userName: string; score: number },
+  entries: RankingEntry[]
+): boolean {
+  const usersEntry = entries.find(
+    (entry) =>
+      entry.score === userData.score && entry.userName === userData.userName
+  );
+  return usersEntry !== undefined;
+}
+
+/**
+ * e-typingにスコアが存在するかどうかを判定する
+ */
+export async function hasEtypingScore({
+  userData,
+  fetchRankingByPage,
+  pageNum,
+}: HasEtypingScoreInput): Promise<boolean> {
+  const page = await findPageByScore({
+    score: userData.score,
+    fetchRankingByPage,
+    pageNum,
+  });
+  if (page === pageNum + 1) {
+    // ランキングにあるスコアの最小値より小さい値を指定した場合
+    return false;
+  }
+
+  const entries = await fetchRankingByPage(page);
+  if (userDataExists(userData, entries)) return true;
+
+  // ユーザーのスコアが対象のページのスコア最小値と同じ場合、次のページにある可能性もあるので探す
+  const minScore = Math.min(...entries.map((entry) => entry.score));
+  if (userData.score === minScore && page < pageNum) {
+    const entries = await fetchRankingByPage(page + 1);
+    if (userDataExists(userData, entries)) return true;
+  }
+
+  return false;
+}
